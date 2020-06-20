4ed461bfd58b582546dfc77344b0e43fd078b93f
gento-ogane <gento.ogane@i-d.ai>
2020-06-21 03:53:27 +0900

    add amplify and contact

--- src/views/Pages/About.tsx
+++ src/views/Pages/About.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import styled from "styled-components";
 import TitleText from "../atoms/TitleText";
 import Avatar from "../../utils/avatar.jpg";
-import Chip from '@material-ui/core/Chip';
-import {headerHeightNum} from "../../utils/size";
+import Chip from "@material-ui/core/Chip";
+import { headerHeightNum } from "../../utils/size";
 
 const About: React.FC = (props) => {
   return (
@@ -14,24 +14,24 @@ const About: React.FC = (props) => {
       <Table>
         <Tbody>
           <tr>
-            <Td>Name</Td>
-            <td>Gento Ogane</td>
+            <Tdl>Name</Tdl>
+            <Tdr>Gento Ogane</Tdr>
           </tr>
           <tr>
-            <Td>Birth</Td>
-            <td>1998.11.24</td>
+            <Tdl>Birth</Tdl>
+            <Tdr>1998.11.24</Tdr>
           </tr>
           <tr>
-            <Td>University</Td>
-            <td>Sophia Unitversity</td>
+            <Tdl>University</Tdl>
+            <Tdr>Sophia University</Tdr>
           </tr>
           <tr>
-            <Td>Major</Td>
-            <td>Information Science</td>
+            <Tdl>lMajor</Tdl>
+            <Tdr>Information Science</Tdr>
           </tr>
           <tr>
-            <Td>Interesting</Td>
-            <td>Web Application</td>
+            <Tdl>Interesting</Tdl>
+            <Tdr>Web Application</Tdr>
           </tr>
         </Tbody>
       </Table>
@@ -40,37 +40,37 @@ const About: React.FC = (props) => {
       {/* <Tooltip title="Lebel3"><Button>HTML</Button></Tooltip> */}
       <p>★★★</p>
       <RowThree>
-      <SkillChip label="React"/>
-      <SkillChip label="Unity(C#)" />
-      <SkillChip label="Amplify" />
-      <SkillChip label="HTML" />
-      <SkillChip label="CSS" />
+        <SkillChip label="React" />
+        <SkillChip label="Unity(C#)" />
+        <SkillChip label="Amplify" />
+        <SkillChip label="HTML" />
+        <SkillChip label="CSS" />
       </RowThree>
       <p>★★</p>
       <RowTwo>
-      <SkillChip label="Python" />
-      <SkillChip label="GAS" />
-      <SkillChip label="Git/GitHub" />
-      <SkillChip label="jQuery" />
-      <SkillChip label="TypeScript" />
+        <SkillChip label="Python" />
+        <SkillChip label="GAS" />
+        <SkillChip label="Git/GitHub" />
+        <SkillChip label="jQuery" />
+        <SkillChip label="TypeScript" />
       </RowTwo>
       <p>★</p>
       <RowOne>
-      <SkillChip label="Flutter" />
-      <SkillChip label="Docker" />
-      <SkillChip label="AWS" />
-      <SkillChip label="Ruby on Rails" />
-      <SkillChip label="WordPress" />
+        <SkillChip label="Flutter" />
+        <SkillChip label="Docker" />
+        <SkillChip label="AWS" />
+        <SkillChip label="Ruby on Rails" />
+        <SkillChip label="WordPress" />
       </RowOne>
     </Wrap>
   );
 };
 
 const Wrap = styled.div`
-  margin-top:${headerHeightNum+"px"};
+  margin-top: ${headerHeightNum + "px"};
   width: 100%;
   text-align: center;
-  padding-bottom:100px;
+  padding-bottom: 100px;
 `;
 const Img = styled.img`
   width: 200px;
@@ -80,46 +80,53 @@ const Img = styled.img`
 const Table = styled.table`
   display: flex;
   justify-content: center;
+  margin: auto;
+  width: 900px;
 `;
 
 const Tbody = styled.tbody`
-  width: 320px;
+  width: 500px;
 `;
 
-const Td = styled.td`
-  width: 52%;
+const Tdl = styled.td`
+  width: 70%;
   text-align: left;
   padding: 10px;
 `;
 
-const RowThree=styled.div`
-  display:flex;
-  justify-content: space-around;
-  align-content: space-around;
-  flex-wrap: wrap; 
-  width: 30%;
-  margin:auto;
-`
-const RowTwo=styled.div`
-  display:flex;
-  justify-content: space-around;
-  align-content: space-around;
-  flex-wrap: wrap; 
-  width: 30%;
-  margin:auto;
-`
-const RowOne=styled.div`
-  display:flex;
-  justify-content: space-around;
-  align-content: space-around;
-  flex-wrap: wrap; 
-  width: 30%;
-  margin:auto;
-`
+const Tdr = styled.td`
+  width: 60%;
+  text-align: left;
+  padding: 10px;
+`;
 
-const SkillChip =styled(Chip)`
-margin:10px;
-`
+const RowThree = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-content: space-around;
+  flex-wrap: wrap;
+  width: 50%;
+  margin: auto;
+`;
+const RowTwo = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-content: space-around;
+  flex-wrap: wrap;
+  width: 50%;
+  margin: auto;
+`;
+const RowOne = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-content: space-around;
+  flex-wrap: wrap;
+  width: 50%;
+  margin: auto;
+`;
 
+const SkillChip = styled(Chip)`
+  margin: 10px;
+`;
 
 export default About;
--- src/views/Pages/Contact.tsx
+++ src/views/Pages/Contact.tsx
@@ -1,19 +1,67 @@
 import * as React from "react";
 import styled from "styled-components";
 import TitleText from "../atoms/TitleText";
-import {headerHeightNum} from "../../utils/size";
+import { headerHeightNum,footerHeightNum } from "../../utils/size";
+import TextField from "@material-ui/core/TextField";
+import {Button as OriginalButton} from "@material-ui/core";
+
+
+const styles = {
+  input1: {
+    height: 50,
+  },
+};
 
 const Contact: React.FC = (props) => {
   return (
     <Wrap>
-      <TitleText variant="h5">Contact</TitleText>
+      <Form>
+        <TitleText variant="h5">Contact</TitleText>
+        <SubjectTextField required label="subject" variant="outlined" />
+        <EmailTextField required label="email" variant="outlined" />
+        <ContentTextField
+          required
+          label="content"
+          variant="outlined"
+          multiline
+          rows={10}
+        />
+        <SubmitButton variant="outlined">Submit</SubmitButton>
+      </Form>
     </Wrap>
   );
 };
 
 const Wrap = styled.div`
- margin-top:${headerHeightNum+"px"};
+  margin-top: ${headerHeightNum + "px"};
+  margin-bottom: ${footerHeightNum + "px"};
   width: 100%;
+  text-align: center;
 `;
+
+const Form = styled.div`
+  width: 70%;
+  margin: auto;
+`;
+
+const SubjectTextField = styled(TextField)`
+  width: 100%;
+  margin: 20px 0px !important;
+`;
+
+const EmailTextField = styled(TextField)`
+  width: 100%;
+  margin: 20px 0px !important;
+`;
+
+const ContentTextField = styled(TextField)`
+  width: 100%;
+  margin: 20px 0px !important;
+`;
+
+const SubmitButton =styled(OriginalButton)`
+   width: 80%;
+  margin: 20px 0px !important;
+`
 
 export default Contact;
