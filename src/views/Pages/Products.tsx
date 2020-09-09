e0ec3ba82bda7b1b7fc84004b1ea298df000b685
gento-ogane <ponpon_1325@yahoo.co.jp>
2020-09-10 02:29:14 +0900

    [WIP]Productページの修正

--- src/views/Pages/Products.tsx
+++ src/views/Pages/Products.tsx
@@ -2,11 +2,14 @@ import * as React from "react";
 import styled from "styled-components";
 import TitleText from "../atoms/TitleText";
 import {headerHeightNum} from "../../utils/size";
+import ProductCard from '../organisms/ProductCard';
+import logo1 from "../../utils/products/product01.jpg";
 
 const Products: React.FC = (props) => {
   return (
     <Wrap>
       <TitleText variant="h5">Products</TitleText>
+      <ProductCard image={logo1} title={"Gentz.dev"} detail={"自身の経歴や作品をまとめたシンプルなポートフォリオサイトです。"}/>
     </Wrap>
   );
 };
---
+++ src/views/organisms/ProductCard.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import {Card,CardActionArea,CardMedia,CardContent,Typography,Button,CardActions} from '@material-ui/core';
+
+interface Props{
+    image:string,
+    title:string,
+    detail:string
+
+}
+
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+  });
+  const ProductCard: React.FC<Props> = props=> {
+    const classes = useStyles();
+    return (
+        <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="220"
+            src={props.image}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {props.detail}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    );
+  };
+  
+  
+  export default ProductCard;
