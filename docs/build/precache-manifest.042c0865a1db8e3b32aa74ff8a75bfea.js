bdde460a7c42d1f35d3837087d59d6025a6ecb36
gento-ogane <ponpon_1325@yahoo.co.jp>
2021-01-23 18:44:53 +0900

    deploy

---
+++ docs/build/precache-manifest.042c0865a1db8e3b32aa74ff8a75bfea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b505d3ed8772e0581120635bb3212651",
+    "url": "https://gento-ogane.github.io/index.html"
+  },
+  {
+    "revision": "86dd95f90c2437c2fb77",
+    "url": "https://gento-ogane.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "494fd5d3f3d985759b3f",
+    "url": "https://gento-ogane.github.io/static/js/2.cf7a366f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "https://gento-ogane.github.io/static/js/2.cf7a366f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86dd95f90c2437c2fb77",
+    "url": "https://gento-ogane.github.io/static/js/main.751d59bf.chunk.js"
+  },
+  {
+    "revision": "5ed7a7589da72f8824b1",
+    "url": "https://gento-ogane.github.io/static/js/runtime-main.292a2f00.js"
+  },
+  {
+    "revision": "44db728e44a5656482933e86dc208bb1",
+    "url": "https://gento-ogane.github.io/static/media/product01.44db728e.jpg"
+  }
+]);
