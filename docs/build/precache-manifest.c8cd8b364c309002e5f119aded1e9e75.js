cf3e6cef4f755df46dce7dd6b45fa0a2c931dde6
gento-ogane <ponpon_1325@yahoo.co.jp>
2021-01-24 13:09:33 +0900

    deploy

---
+++ docs/build/precache-manifest.c8cd8b364c309002e5f119aded1e9e75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e6f01bf4f7c11262c4f7a084dc98592",
+    "url": "/gentz/index.html"
+  },
+  {
+    "revision": "ed8c0a704fb526a6660d",
+    "url": "/gentz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "847978a5dd019c5e7bf4",
+    "url": "/gentz/static/js/2.b48e0e84.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/gentz/static/js/2.b48e0e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8c0a704fb526a6660d",
+    "url": "/gentz/static/js/main.fa207b1a.chunk.js"
+  },
+  {
+    "revision": "f99cff3272662a022c2c",
+    "url": "/gentz/static/js/runtime-main.3729fe00.js"
+  },
+  {
+    "revision": "44db728e44a5656482933e86dc208bb1",
+    "url": "/gentz/static/media/product01.44db728e.jpg"
+  }
+]);
