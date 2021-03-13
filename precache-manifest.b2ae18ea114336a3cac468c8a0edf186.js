879fd37139aeb8c87996e09a64cdb8f20bd1331b
gento-ogane <ponpon_1325@yahoo.co.jp>
2021-03-13 13:33:19 +0900

    Updates

---
+++ precache-manifest.b2ae18ea114336a3cac468c8a0edf186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671c8a92ad0089f451c8b69d4ef7c93f",
+    "url": "/gentz/index.html"
+  },
+  {
+    "revision": "e84b3c6695da6b078604",
+    "url": "/gentz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "847978a5dd019c5e7bf4",
+    "url": "/gentz/static/js/2.b48e0e84.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/gentz/static/js/2.b48e0e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84b3c6695da6b078604",
+    "url": "/gentz/static/js/main.c3e68937.chunk.js"
+  },
+  {
+    "revision": "f99cff3272662a022c2c",
+    "url": "/gentz/static/js/runtime-main.3729fe00.js"
+  },
+  {
+    "revision": "44db728e44a5656482933e86dc208bb1",
+    "url": "/gentz/static/media/product01.44db728e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5866b62690129add5f556ef6467be5b4.js"
+  "/gentz/precache-manifest.b2ae18ea114336a3cac468c8a0edf186.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gentz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
