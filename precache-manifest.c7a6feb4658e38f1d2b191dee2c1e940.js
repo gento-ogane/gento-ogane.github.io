439ee91631adcc404f1180c6fa68bea511168813
gento-ogane <ponpon_1325@yahoo.co.jp>
2021-03-13 13:09:21 +0900

    Updates

---
+++ precache-manifest.c7a6feb4658e38f1d2b191dee2c1e940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f486fc577e132d4d12aafaeaccf10f75",
+    "url": "/gentz/index.html"
+  },
+  {
+    "revision": "312d6c6f9e991e38ae2c",
+    "url": "/gentz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "847978a5dd019c5e7bf4",
+    "url": "/gentz/static/js/2.b48e0e84.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/gentz/static/js/2.b48e0e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312d6c6f9e991e38ae2c",
+    "url": "/gentz/static/js/main.5b58850f.chunk.js"
+  },
+  {
+    "revision": "f99cff3272662a022c2c",
+    "url": "/gentz/static/js/runtime-main.3729fe00.js"
+  },
+  {
+    "revision": "44db728e44a5656482933e86dc208bb1",
+    "url": "/gentz/static/media/product01.44db728e.jpg"
+  }
+]);
